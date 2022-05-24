a6ab1adae5b92fc1a3af1338579bdaf76b5e089a
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-05-24 23:04:30 +0100

    [fix] adjust cart modal structure

--- src/components/Header/Cart/CartModal.tsx
+++ src/components/Header/Cart/CartModal.tsx
@@ -8,9 +8,10 @@ import ProductSideBar from '../../ProductSideBar.tsx/ProductSideBar'
 const CartModal = () => {
     const navigate = useNavigate()
     return (
-        <section className='relative w-screen'>
+        <section className='relative w-screen max-w-[420px] '>
             <Overlay menuOpen={true} handleClick={() => navigate(-1)} />
-            <div className='w-screen max-w-[420px]  fixed top-0 right-0 h-screen bg-white pt-[15px] md:[pt-30px]  z-[1100] py-[13px] md:py-[30px]'>
+            <div className='w-screen max-w-[500px]  fixed top-0 right-0 h-screen bg-white pt-[15px] md:[pt-30px]  z-[1100] py-[13px] md:py-[30px] flex flex-col'>
+
                 <div className='flex justify-between items-center  gap-4 px-3 lg:px-7'>
                     <div className='flex-grow flex items-end gap-1'>
                         <span className='text-[32px] inline-block'>
@@ -24,33 +25,34 @@ const CartModal = () => {
                     <CloseIcon handleClick={() => navigate(-1)} />
                 </div>
 
-                <div className='h-[320px] my-auto overflow-y-scroll mt-5 py-4 flex flex-col gap-10 justify-around px-3 lg:px-7  '>
+                <div className=' flex-grow overflow-hidden flex flex-col'>
+                    <div className='flex-shrink flex-grow-0 h-full  my-auto overflow-y-scroll mt-5 py-4 divide-y-[20px] divide-white px-3 lg:px-7  '>
 
-                    {/* product  */}
-                  <ProductSideBar/>
-                  <ProductSideBar/>
-                  <ProductSideBar/>
-                  <ProductSideBar/>
-                  <ProductSideBar/>
-                  <ProductSideBar/>
-                  <ProductSideBar/>
-                {/* product  */}
-                </div>
-                <div>
-                    <div className='flex flex-col  gap-4 shadow-[0_-2px_5px_0px_rgba(0,0,0,0.1)]  py-6 px-3 lg:px-7 absolute bottom-0 left-0 w-full bg-white'>
+                        {/* product  */}
+                        <ProductSideBar />
+                        <ProductSideBar />
+                        <ProductSideBar />
+                        <ProductSideBar />
+                        <ProductSideBar />
+                        <ProductSideBar />
+                        <ProductSideBar />
+                        {/* product  */}
+                    </div>
+<div className='flex flex-col  gap-4 shadow-[0_-2px_5px_0px_rgba(0,0,0,0.1)]  py-6 px-3 lg:px-7 bottom-0 left-0 w-full bg-white flex-grow'>
                         <div className='flex justify-between '>
 
-                    <p>subtotal</p>
-                    <p className='text-[16x] leading-none  p-0 block ' >₦42140</p>
+                            <p>subtotal</p>
+                            <p className='text-[16x] leading-none  p-0 block ' >₦42140</p>
                         </div>
-                    <button className='text-white py-3 bg-secondary-600 w-full block'>
+                        <button className='text-white py-3 bg-secondary-600 w-full block'>
                             Proceed to Checkout
                         </button>
-                    </div>
+                    </div> 
 
-                   
                 </div>
+
             </div>
+
         </section>
     )
 }
