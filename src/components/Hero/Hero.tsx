018b6ab511214d70c4d133e5ed11fb8d195ef5c6
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-05-29 13:57:56 +0100

    [feature] adjust video playback

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useLayoutEffect, useEffect } from 'react'
 import Button from '../Button';
 
 
@@ -6,23 +6,23 @@ import Button from '../Button';
 
 const Hero = () => {
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         const w = window.matchMedia("(max-width: 700px)");
         const vid = document.getElementById("video") as HTMLVideoElement
 
         if (w.matches) {
-            vid.pause();
+            // vid.pause();
             // source.removeAttribute("src");
             // source.setAttribute("src", "https://storage.googleapis.com/coverr-main/mp4/Love-Boat.mp4");
             vid.setAttribute("src", "https://ik.imagekit.io/haybececodes/mobile-hero-video_720x1280_2kqek7_j4.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387652871");
-            vid.load();
-            vid.play();
+            // vid.load();
+            // vid.play();
         } else {
-            vid.pause();
+            // vid.pause();
             // source.removeAttribute("src");
             vid.setAttribute("src", "https://ik.imagekit.io/haybececodes/desktop-hero-video_854x480_bpPIjPIB9.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387638028");
-            vid.load();
-            vid.play();
+            // vid.load();
+            // vid.play();
         }
     }, [])
     return (
