5159d18af2c0122a7dfb6b1b5ab9c1cb17f22729
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-05-29 20:38:45 +0100

    [fix] makes video play on first load

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,37 +1,58 @@
-import React, { useLayoutEffect, useEffect } from 'react'
+import React, { useState, useLayoutEffect, useEffect } from 'react'
 import Button from '../Button';
+import Mobile from "../../assets/videos/mobile.mp4"
+import Desktop from "../../assets/videos/desktop.mp4"
 
 
+const getVideoSrc = (width: number) => {
+    if (width >= 768) return Desktop
+    if (width < 768) return Mobile
+}
 
 
 const Hero = () => {
+    const [isVideoLoaded, setIsVideoLoaded] = useState(false)
+    const src = getVideoSrc(window.innerWidth)
+    const onLoadedData = () => {
+        setIsVideoLoaded(true);
+    }
 
-    useLayoutEffect(() => {
+    // useLayoutEffect(() => {
+    //     const w = window.matchMedia("(max-width: 700px)");
+    //     const vid = document.getElementById("video") as HTMLVideoElement
+
+    //     if (w.matches) {
+    //         vid.pause();
+    //         // source.removeAttribute("src");
+    //         // source.setAttribute("src", "https://storage.googleapis.com/coverr-main/mp4/Love-Boat.mp4");
+    //         vid.setAttribute("src", "https://ik.imagekit.io/haybececodes/mobile-hero-video_720x1280_2kqek7_j4.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387652871");
+    //          vid.load();
+    //         vid.play();
+    //     } else {
+    //          vid.pause();
+    //         // source.removeAttribute("src");
+    //         vid.setAttribute("src", "https://ik.imagekit.io/haybececodes/desktop-hero-video_854x480_bpPIjPIB9.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387638028");
+    //          vid.load();
+    //          vid.play();
+    //     }
+    // }, [])
+    useEffect(() => {
         const w = window.matchMedia("(max-width: 700px)");
-        const vid = document.getElementById("video") as HTMLVideoElement
+        const vid = document.querySelectorAll("video")
+        vid.forEach(v => v.autoplay = true)
 
-        if (w.matches) {
-            vid.pause();
-            // source.removeAttribute("src");
-            // source.setAttribute("src", "https://storage.googleapis.com/coverr-main/mp4/Love-Boat.mp4");
-            vid.setAttribute("src", "https://ik.imagekit.io/haybececodes/mobile-hero-video_720x1280_2kqek7_j4.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387652871");
-             vid.load();
-            vid.play();
-        } else {
-             vid.pause();
-            // source.removeAttribute("src");
-            vid.setAttribute("src", "https://ik.imagekit.io/haybececodes/desktop-hero-video_854x480_bpPIjPIB9.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387638028");
-             vid.load();
-             vid.play();
-        }
     }, [])
     return (
         <div className='md:flex-grow flex-shrink-0 bg-green-600 relative overflow-hidden '>
             <div className='w-full md:aspect-video lg:absolute top-0 left-0 right-0 bottom-0 h-full md:object-contain lg:-translate-y-1/4 hidden md:block'>
-                <video id="video" autoPlay={true} preload="auto" src="https://ik.imagekit.io/haybececodes/mobile-hero-video_720x1280_2kqek7_j4.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387652871" loop className='w-full '></video>
+                {/* <img src={Mobile} className="" /> */}
+                <video src={Mobile} muted={false} autoPlay={true} preload="auto" className='video w-full' playsInline />
+
+
             </div>
             <div className='w-full md:aspect-video lg:absolute top-0 left-0 right-0 bottom-0 h-full md:object-contain lg:-translate-y-1/4 block md:hidden'>
-                <video id="video" autoPlay={true} preload="auto" src="https://ik.imagekit.io/haybececodes/mobile-hero-video_720x1280_2kqek7_j4.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387652871" loop className='w-full '></video>
+                <video id="video" autoPlay muted loop className='video w-full ' playsInline src={Desktop} />
+                {/* <img src={Desktop} className="" /> */}
             </div>
             <div>
                 <div className='absolute top-0 left-0 right-0 bottom-0 z-[900] bg-black/25'>
@@ -40,11 +61,11 @@ const Hero = () => {
                 <div className='absolute top-1/2 left-1/2 -translate-x-1/2 text-white -translate-y-1/2 z-[950] w-full text-center'>
                     <h1 className='font-extrabold text-3xl md:text-6xl lg:text-7xl'>Your body deserves better</h1>
                     <p className='font-light text-xs lg:text-sm '> Let's go get what you deserve , Dear.</p>
-                     <div className='inline-block mt-3 lg:mt-10'> 
+                    <div className='inline-block mt-3 lg:mt-10'>
                         <Button classname='inline-block px-16'>
                             Start Shopping
                         </Button>
-                     </div> 
+                    </div>
                 </div>
             </div>
 
