6ee4b4d5b9defccad7ce12709e7b1019af31465b
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-05-29 12:55:50 +0100

    [feature] adds hero video

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex-grow  flex-shrink-0 bg-green-500'>
+    <div><video      autoPlay={true} preload="auto" src="https://ik.imagekit.io/haybececodes/mobile-hero-video_720x1280_2kqek7_j4.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387652871" loop></video>
+            {/* <source src='https://ik.imagekit.io/haybececodes/mobile-hero-video_720x1280_2kqek7_j4.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387652871' type='video/mp4' /> */}
+        </div>
+        
+        Hero</div>
+  )
+}
+
+export default Hero
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header'
+import Hero from '../../components/Hero'
 
 const Home = () => {
   return (
     <>
+    <div className='flex flex-col  h-screen'>
       <Header />
+      <Hero/>
+    </div>
     </>
     // <div>Home</div>
   )
