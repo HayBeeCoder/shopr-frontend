5597681646424d8b5fb196fbc885dfb6dad88147
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-05-29 14:00:12 +0100

    [feature] sighs

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -31,10 +31,10 @@ const Hero = () => {
                 <video id="video" autoPlay={true} preload="auto" src="https://ik.imagekit.io/haybececodes/mobile-hero-video_720x1280_2kqek7_j4.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1652387652871" loop className='w-full '></video>
             </div>
             <div>
-                <div className='absolute top-0 left-0 right-0 bottom-0 z-[1300] bg-black/25'>
+                <div className='absolute top-0 left-0 right-0 bottom-0 z-[900] bg-black/25'>
 
                 </div>
-                <div className='absolute top-1/2 left-1/2 -translate-x-1/2 text-white -translate-y-1/2 z-[1350] w-full text-center'>
+                <div className='absolute top-1/2 left-1/2 -translate-x-1/2 text-white -translate-y-1/2 z-[950] w-full text-center'>
                     <h1 className='font-extrabold text-3xl md:text-6xl lg:text-7xl'>Your body deserves better</h1>
                     <p className='font-light text-xs lg:text-sm '> Let's go get what you deserve , Dear.</p>
                      <div className='inline-block mt-4 lg:mt-10'> 
