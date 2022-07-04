0f821cfe15144d4439b17cfb079b4caac88fed40
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-07-04 04:17:46 -0700

    [fix] adjusts image in sign up page when in Ipad view

--- src/features/auth/Layout.tsx
+++ src/features/auth/Layout.tsx
@@ -7,12 +7,12 @@ import WomanJeans from "../../assets/images/woman-jeans.jpg"
 const Layout = () => {
   return (
     <div className='w-screen h-screen flex'>
-      <div className='hidden md:block w-1/2 h-full bg-secondary-400 relative right-0 overflow-hidden '>
+      <div className='hidden md:block w-1/2 h-full bg-secondary-400 relative right-0 overflow-hidden'>
     {/* <img src={WomanJeans} alt="" className='w-full object-cover relative -translate-y-1/4' /> */}
-        <picture>
+        <picture >
           <source srcSet={Model} media="(min-width: 768px)"/>
-          <img src="" alt=""/>
-        </picture>
+          <img src="" alt="" className='md:object-cover md:h-full w-full'/>
+        </picture> 
       </div>
       <div className='w-full md:w-1/2'>
         <div className=' text-center mt-7 mb-12'>
